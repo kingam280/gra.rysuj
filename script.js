dac37b7281677657160ca5a9dbd5f05fce03ccdd
kingam280 <57267201+kingam280@users.noreply.github.com>
2020-09-23 16:09:29 +0200

    Add files via upload
    Added colors and instruction.

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-const divs = document.querySelectorAll('div>div');
-const btn = document.querySelector('button');
+const divs = document.querySelectorAll('.wrapper>div');
+const btn = document.querySelector('.clear');
+const colors = document.querySelectorAll('div>button')
+const redBtn = document.querySelector('.red')
+const yellowBtn = document.querySelector('.yellow')
 
-function changeColor() {
-    this.classList.toggle('black');
-}
+//rysowanie różnymi kolorami
+colors.forEach(function (div) {
+    div.addEventListener('click', function () {
+        let color = this.classList;
+        divs.forEach(function (div) {
+            div.addEventListener('click', function () {
+                this.className = color;
+            });
+        })
+    });
 
-divs.forEach(function (div) {
-    div.addEventListener('click', changeColor);
+
 })
 
+//wymazywanie
+divs.forEach(function (div) {
+    div.addEventListener('dblclick', function () {
+        this.removeAttribute('class');
+
+    });
+})
+
+//czyszczenie tablicy
 btn.addEventListener('click', function () {
     divs.forEach(function (div) {
-        div.classList.remove('black')
+        div.removeAttribute('class');
     })
 })
+
+//pop up
+function modal() {
+    document.querySelector('.modal-wrapper').classList.toggle('active');
+    document.querySelector('.all').classList.toggle('blur');
+}
+
+document.querySelector('.instruction').addEventListener('click', modal)
+document.querySelector('.close').addEventListener('click', modal)
